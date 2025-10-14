a7a50369123fa24c3ebc4a13049c578e9fe6856a
abuXahra <abdulmuminisah@yahoo.com>
2025-10-14 10:05:44 +0100

    updated

--- routes/product.js
+++ routes/product.js
@@ -4,6 +4,7 @@ const productController = require("../controller/productController");
 
 router.post("/create", productController.createProduct);
 router.get("/", productController.fetchProducts);
+router.get("/low-stock", productController.fetchLowStockProducts);
 // add bulk delete route before the dynamic route
 router.delete("/bulk-delete", productController.bulkDeleteProducts);
 router.get("/:productId", productController.fetchProduct);
